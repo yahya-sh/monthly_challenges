775dab7c83415633713c721ab30ad50ffcce3e5c
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-05-31 17:46:18 +0300

    Add header.html that can be included across all pages

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name='index'),
     path("<int:month>/", views.month_by_number),
     path("<str:month>/", views.month_by_str, name='monthly-challenge'),
 ]
